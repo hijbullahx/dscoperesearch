ddca613a0e25e292aa07d50a6e7ae49dc4598a1b
hijbullahx <hijbullah119445@gmail.com>
2026-04-08 22:29:06 +0600

    nav adding

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -8,3 +8,15 @@ class Project(models.Model):
 
   def __str__(self):
     return self.title
+
+
+# Team member model for admin and public display
+class TeamMember(models.Model):
+  name = models.CharField(max_length=100)
+  role = models.CharField(max_length=100)
+  bio = models.TextField(blank=True)
+  photo = models.ImageField(upload_to='team_photos/', blank=True, null=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.name
