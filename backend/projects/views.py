5097d06ac49a3d31622b8da8901e8f0e0bde287e
hijbullahx <hijbullah119445@gmail.com>
2026-04-08 19:44:50 +0600

    views for project

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Project
 
-# Create your views here.
+@api_view(['GET'])
+def project_list(request):
+  projects = Project.objects.all()
+
+  data = []
+  for project in projects:
+    data.append({
+      "id": project.id,
+      "title": project.title,
+      "description": project.description,
+      "github_link" : project.github_link,
+    })
+
+  return Response(data)
